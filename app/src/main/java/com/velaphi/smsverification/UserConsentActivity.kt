d8ce26feca25cf81cb1686962a46cf0bd4cb32a9
velaphim <48346024+velaphim@users.noreply.github.com>
2019-12-12 09:50:38 +0200

    git the user consent working

--- app/src/main/java/com/velaphi/smsverification/UserConsentActivity.kt
+++ app/src/main/java/com/velaphi/smsverification/UserConsentActivity.kt
@@ -1,17 +1,25 @@
 package com.velaphi.smsverification
 
+import android.app.Activity
+import android.content.*
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.phone.SmsRetriever
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.common.api.Status
 import com.velaphi.smsverification.utils.SmsBroadcastReceiver
 import com.velaphi.smsverification.utils.SmsListener
 import kotlinx.android.synthetic.main.activity_user_consent.*
+import java.io.IOException
+import java.util.*
+import java.util.regex.Pattern
 
 class UserConsentActivity : AppCompatActivity() {
 
     private var sender: Boolean = false
+    private val TAG = UserConsentActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +27,10 @@ class UserConsentActivity : AppCompatActivity() {
         toggleNumber()
         setToolbar()
         start_button.setOnClickListener {
-            if (sender) {
-                if (!phone_number_edit_text.text.isNullOrEmpty())
-                    withSenderNumber(phone_number_edit_text.text.toString())
-            } else {
-                withoutSenderNumber()
-            }
+            //            if(smsVerificationReceiver != null){
+//                unregisterReceiver(smsVerificationReceiver)
+//            }
+            startOtpValidation()
         }
 
     }
@@ -35,35 +41,6 @@ class UserConsentActivity : AppCompatActivity() {
         actionBar?.title = getString(R.string.sms_user_consent_api)
     }
 
-    private fun withSenderNumber(number: String) {
-        val client = SmsRetriever.getClient(this)
-
-        val task = client.startSmsUserConsent(number)
-
-        task.addOnSuccessListener {
-            Log.d("CodeActivity", "Sms listener started!")
-            listenSms()
-        }
-
-        task.addOnFailureListener { e ->
-            Log.e("CodeActivity", "Failed to start sms retriever: ${e.message}")
-        }
-    }
-
-    private fun withoutSenderNumber() {
-        val client = SmsRetriever.getClient(this)
-
-        val task = client.startSmsRetriever()
-
-        task.addOnSuccessListener {
-            Log.d("CodeActivity", "Sms listener started!")
-            listenSms()
-        }
-
-        task.addOnFailureListener { e ->
-            Log.e("CodeActivity", "Failed to start sms retriever: ${e.message}")
-        }
-    }
 
     private fun toggleNumber() {
         number_radio_group.setOnCheckedChangeListener { _, checkedId ->
@@ -72,10 +49,10 @@ class UserConsentActivity : AppCompatActivity() {
                     phone_number_input_layout.visibility = View.VISIBLE
                     sender = true
                 }
-
                 else -> {
                     phone_number_input_layout.visibility = View.GONE
                     sender = false
+                    phone_number_edit_text.text?.clear()
                 }
             }
         }
@@ -86,15 +63,89 @@ class UserConsentActivity : AppCompatActivity() {
         return true
     }
 
-    private fun listenSms() {
-        SmsBroadcastReceiver.bindListener(object : SmsListener {
-            override fun onSuccess(code: String) {
-                otp_edit_text_input.setText(code)
-            }
+    private fun startOtpValidation() {
+        otpSmsTask()
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        Objects.requireNonNull(this)
+            .registerReceiver(smsVerificationReceiver, intentFilter)
+    }
 
-            override fun onError() {
-                // TODO: Display error message
+
+    private fun otpSmsTask() {
+        val number: String?
+
+        if (!phone_number_edit_text.text.isNullOrEmpty() && sender) {
+            number = phone_number_edit_text.text.toString()
+        } else {
+            number = null
+        }
+
+        val task = SmsRetriever.getClient(this).startSmsUserConsent(number)
+
+        task.addOnCompleteListener { listener ->
+            if (listener.isSuccessful) {
+                Log.d(TAG, "otpSmsTask: OTP Listener Successful")
+            } else {
+                val exception = listener.exception
+                Log.e(TAG, "otpSmsTask: OTP Listener failed", exception)
             }
-        })
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SMS_CONSENT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val smsMessage = data!!.getStringExtra(SmsRetriever.EXTRA_SMS_MESSAGE)
+                if (smsMessage != null) {
+                    val oneTimePin = parseOneTimePinSms(smsMessage)
+                    if (!oneTimePin.isNullOrEmpty()) {
+                        otp_edit_text_input.setText(oneTimePin)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun parseOneTimePinSms(message: String): String? {
+        val pattern = Pattern.compile(OTP_SMS_REGEX)
+        val matcher = pattern.matcher(message)
+        var oneTimePin: String? = null
+
+        if (matcher.find()) {
+            oneTimePin = matcher.group()
+        }
+
+        return oneTimePin
+    }
+
+    private val smsVerificationReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            if (SmsRetriever.SMS_RETRIEVED_ACTION == intent.action) {
+                val extras = intent.extras
+                val smsRetrieverStatus = extras!!.get(SmsRetriever.EXTRA_STATUS) as Status
+
+                if (smsRetrieverStatus.statusCode == CommonStatusCodes.SUCCESS) {
+                    val consentIntent =
+                        extras.getParcelable<Intent>(SmsRetriever.EXTRA_CONSENT_INTENT)
+                    try {
+                        startActivityForResult(consentIntent, SMS_CONSENT_REQUEST)
+                    } catch (exception: ActivityNotFoundException) {
+                        Log.e(TAG, "BroadcastReceiver TIMEOUT", exception)
+                    }
+
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        this.unregisterReceiver(smsVerificationReceiver)
+    }
+
+    companion object {
+        const val SMS_CONSENT_REQUEST = 2
+        const val OTP_SMS_REGEX = "\\d{8}"
     }
 }
